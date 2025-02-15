b0e3893ce71dc9d5d9c19aa0bee58f4318645603
dausita <delikanyagiz.35@gmail.com>
2025-02-15 16:05:39 +0300

    Update LemoniumHub.lua

--- LemoniumHub.lua
+++ LemoniumHub.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 local Window = Fluent:CreateWindow({
     Title = "Lemonium Hub " .. Fluent.Version,
-    SubTitle = "by dausita",
+    SubTitle = "by dausita helper wallyxlv",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
