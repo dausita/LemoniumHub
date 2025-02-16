90cc8ac67c687352e05cd57f726964ec2d097302
dausita <delikanyagiz.35@gmail.com>
2025-02-16 16:15:35 +0300

    Update LemoniumHub.lua

--- LemoniumHub.lua
+++ LemoniumHub.lua
@@ -302,6 +302,39 @@ Tabs.Misc:AddButton({
     end
 })
 
+Tabs.Misc:AddSlider("headSizeChanger", 
+{
+    Title = "Head Size Changer",  
+    Description = "Changes the head size of other players",
+    Default = 15,
+    Min = 10,
+    Max = 30,
+    Rounding = 1,
+    Callback = function(Value)
+        _G.HeadSize = Value  -- Sliderdan gelen değeri alıp HeadSize'a ayarlıyoruz
+        print("Head size changed to:", Value)
+    end
+})
+
+_G.Disabled = true
+
+game:GetService('RunService').RenderStepped:connect(function()
+    if _G.Disabled then
+        for i, v in next, game:GetService('Players'):GetPlayers() do
+            if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+                pcall(function()
+                    -- Diğer oyuncuların HumanoidRootPart boyutunu ve özelliklerini değiştiriyoruz
+                    v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
+                    v.Character.HumanoidRootPart.Transparency = 0.7
+                    v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Royal purple")
+                    v.Character.HumanoidRootPart.Material = "Neon"
+                    v.Character.HumanoidRootPart.CanCollide = false
+                end)
+            end
+        end
+    end
+end)
+
 Tabs.Settings:AddButton({
     Title = "Save Config", 
     Description = "Save your current settings to a config file",
